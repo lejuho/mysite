36f77f67cab6ff3de1d033a2a5922dec7b99ebbc
lejuho <zedd1324@gmail.com>
2024-03-07 19:19:11 +0900

    desktop 1

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.utils import timezone,datetime
+from django.utils import timezone
+from django.utils import datetime
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
